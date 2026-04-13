f1fec43c77a27259675fa8116e09a8029450323d
Braxon2 <dusanb80@gmail.com>
2026-04-13 13:44:54 +0200

    Modified Login page and CSS for that page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,39 +22,28 @@ const Login = () => {
   }, [user, navigate]);
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <h2>LOG IN</h2>
-        </div>
-        <div className="input-container">
-          <p>Email:</p>
-        </div>
-        <div className="input-container">
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="ex. email@gmail.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="input-container">
-          <p>Password</p>
-        </div>
-        <div className="input-container">
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button>Log in</button>
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Log In</h2>
+
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="ex. email@gmail.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Log in</button>
+
         {error && <div className="error">{error}</div>}
-        {user && <div>{user.id}</div>}
       </form>
     </div>
   );
